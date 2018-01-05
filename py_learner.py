2ee99f2d2556cfd1f7572fe656814270d7ecb804
yutaka <p7dea87c7e4n7j@gmail.com>
2018-01-05 23:17:14 +0900

    add differential in output layer

--- py_learner.py
+++ py_learner.py
@@ -8,20 +8,6 @@
 from sklearn.utils import shuffle
 import numpy as np
 
-def softmax_dist_func(input_array):
-    #denominator of softmax function
-    #input_array is {x1, x2, ........, xn}
-    #return is {sum(exp(x1)), sum(exp(x2)), ...... ,sum(exp(xn))}
-    return np.sum(np.exp(input_array), axis=0)
-
-def softmax_func(connections, input_array):
-    #connections is wMK = {w1K, w2K, ......, wmK} m is cluster num, 2 dim
-    #wmK = {wm1, wm2,... wmk} k is node num, 1 dim
-    #inputs is xK = {x1, x2, ......., xk} k is node num
-    #return is {exp(w1KTxK) / sum(exp(w2KTxK)), ....., exp(wmKTxK) / sum(exp(wMKTxK))}
-    inputs = np.matual(connections.T, input_array) #w1KTxK, w2KTxK, ...wmKTxK
-    return np.exp(inputs) / softmax_dist_func(inputs)  #p(cluster1), p(cluster2)...p(clusterm)
-
 def multiclass_cross_entropy(true_class, connections, inputs):    
     np.log(softmax_func(connections, inputs))
     return 0
@@ -29,20 +15,74 @@ def multiclass_cross_entropy(true_class, connections, inputs):
 class SigmoidNetwork:
     '''Neural network which have Sigmoid function as activation function.'''
 
-    def __init__(self, node_num, layer_num):
-        self.connections = np.rondom.uniform(low=-0.1, high=0.1, size(node_num, layer_num)).astype('float32')
-
-    def foward_propagate(input)
+    # * - * - * - * 
+    #   \   /   /   \
+    # * - * - * - * - *
+    #   /   /   \   /
+    # * - * - * - * - *
+    #   \   \   /   / ^
+    # * - * - * - *   |
+    # ^   ^   ^   ^   last layer is output layer
+    # |   |   |   |
+    #     layers
+    
+    def __init__(self, nodes_num, layers_num, classes_num, random_seed):
+        self.properties = {'nodes_num': nodes_num, 'layers_num': layers_num, \
+                           'classes_num': classes_num, 'random_seed': random_seed}
+        self.random_generator = np.random.RandomState(self.properties['random_seed'])
+        self.connections = \
+            self.random_generator.uniform( \
+                low=-0.1, high=0.1, size=(self.properties['layers_num'], \
+                                          self.properties['nodes_num'], \
+                                          self.properties['nodes_num'])).astype('float32')
+        self.classification_connection = \
+            self.random_generator.uniform( \
+                low=-0.1, high=0.1, size=(self.properties['nodes_num'], \
+                                          self.properties['classes_num'])).astype('float32')
+        self.biases = np.zeros((self.properties['nodes_num'], \
+                                self.properties['layers_num'])).astype('float32')
         
-    def sigmoid(x):
+    def forward_propagate(self, input):
+        nodes_num = self.properties['nodes_num']
+        assert input.shape == (nodes_num,), 'Invalid dimension input!!'
+        outputs = np.matrix(np.empty((nodes_num, 0))).astype('float32')
+        for layer in range(self.properties['layers_num']):
+            output = np.dot(input.T, self.connections[layer])
+            output = self.sigmoid(output)
+            rand_array = self.random_generator.rand(nodes_num)
+            output = np.less(rand_array, output).astype(int)
+            outputs = np.append(outputs, output.reshape(1,-1).T, axis=1)
+        return outputs
+
+    def classify(self, input):
+        # input is column vector
+        assert input.shape[0] == self.properties['nodes_num'], 'Invalid dimension input!!'
+        classify_probs = self.softmax_func(self.classification_connection, input)
+        return classify_probs
+        
+    def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
     
-    def deriv_sigmoid(x):
+    def deriv_sigmoid(self, x):
         return sigmoid(x) * (1 - sigmoid(x))    
 
-    
-if __name__ == '__main__':
+    def softmax_func(self, connections, input_array):
+        #connections is wMK = {w1K, w2K, ......, wmK} m is cluster num, 2 dim, line vector
+        #wmK = {wm1, wm2,... wmk} k is node num, 1 dim, column vector
+        #inputs is xK = {x1, x2, ......., xk} k is node num, column vector
+        #return is {exp(w1KTxK) / sum(exp(w2KTxK)), ....., exp(wmKTxK) / sum(exp(wMKTxK))}
+        inputs = np.matmul(connections.T, input_array) #w1KTxK, w2KTxK, ...wmKTxK
+        return np.exp(inputs) / self.softmax_dist_func(inputs)  #p(cluster1), p(cluster2)...p(clusterm)
 
-    numpy.random.seed(43)
-    
-    print('py_learn')
+    def softmax_dist_func(self, input_array):
+        #denominator of softmax function
+        #input_array is {x1, x2, ........, xn}, column vector
+        #return is {sum(exp(x1)), sum(exp(x2)), ...... ,sum(exp(xn))}, scholar
+        return np.sum(np.exp(input_array), axis=0)
+
+    def differential_in_output(self, inp, classify_probs, answer_node):
+        # both input array is column vector.
+        differential =  -np.dot(inp, classify_probs.T)
+        differential[answer_node, :] += 1
+        return differential
+        
